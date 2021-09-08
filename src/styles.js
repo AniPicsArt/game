30855cd663133d5530c440b6639a019cbc5c7ca9
AniPicsArt <anigulqanyan1995@gmail.com>
2021-09-08 10:46:15 +0400

    remove odd styles

--- src/styles.js
+++ src/styles.js
@@ -27,22 +27,6 @@ const useStyles = createUseStyles({
   App: {
     textAlign: 'center',
   },
-  AppLogo: {
-    height: '40vmin',
-  },
-  AppHeader: {
-    backgroundColor: '#282c34',
-    minHeight: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 'calc(10px + 2vmin)',
-    color: '#ffffff',
-  },
-  AppLink: {
-    color: '#09d3ac',
-  }
 })
 
 export default useStyles;
